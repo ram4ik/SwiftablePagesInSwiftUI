fc128d41e26768759263b76103e162d41da29f27
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-11-11 22:30:33 +0200

    Initial Commit

---
+++ SwiftablePagesInSwiftUI/SwiftablePagesInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftablePagesInSwiftUIApp.swift
+//  SwiftablePagesInSwiftUI
+//
+//  Created by Ramill Ibragimov on 11.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftablePagesInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
